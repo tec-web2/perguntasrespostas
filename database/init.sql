23f6c1f839253e02fe402f304f39f894c5336c73
Inacio Filho <inaciofilho.lima@gmail.com>
2024-07-30 08:35:45 -0300

    Adicionando script para o início do banco

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,24 @@
+-- mysql -h localhost -u root -p
+
+
 CREATE DATABASE guiaperguntas;
+USE guiaperguntas
+DROP TABLE IF EXISTS resposta;
+DROP TABLE IF EXISTS pergunta;
+
+CREATE TABLE pergunta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE resposta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    texto TEXT NOT NULL,
+    likes INT DEFAULT 0,
+    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    perguntaId INT,
+    FOREIGN KEY (perguntaId) REFERENCES pergunta(id) ON DELETE CASCADE
+);
