76b6177672fccaf2b75b17763192575b64625597
Inacio Filho <inaciofilho.lima@gmail.com>
2024-08-06 20:13:02 -0300

    Adicionando editar resposta

--- src/controllers/respostas/editarRespotaController.js
+++ src/controllers/respostas/editarRespotaController.js
@@ -1,8 +1,14 @@
 const editarRespostaService = require("../../services/respostas/editarRespostaService");
+const lerRespostaPorID = require("../../services/respostas/lerRespostaPorIDService");
 
 async function editarRepostaController(req, res) {
     const respostaID = req.params.id;
     const { texto } = req.body;
+    const resposta = await lerRespostaPorID(respostaID)
+    if (resposta.autor.dataValues.id !=req.user_id){
+        return res.status(400).send("Usuário errado")
+    }
+
     const novaResposta = editarRespostaService(respostaID, texto)
 
     res.end();
