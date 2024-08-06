0ad0620a70b71fb0af0f8135c0ba163d620a48c4
Inacio Filho <inaciofilho.lima@gmail.com>
2024-08-06 20:09:04 -0300

    Adicionando remover resposta

--- src/services/respostas/lerRespostaPorIDService.js
+++ src/services/respostas/lerRespostaPorIDService.js
@@ -1,12 +1,36 @@
-const { Pergunta, Resposta} = require("../../../database/associations"); 
+const { Resposta, Usuario, RespostaLike } = require("../../../database/associations");
 
-async function lerRespostaPorID(respostaId){
-    return await Resposta.findByPk(respostaId).then((resposta) => {
+async function lerRespostaPorID(respostaId) {
+    try {
+        const resposta = await Resposta.findOne({
+            where: { id: respostaId },
+            include: [
+                {
+                    model: Usuario,
+                    as: "autor", // Inclui o usuário que criou a resposta
+                },
+                {
+                    model: RespostaLike,
+                    as: "likes", // Inclui as curtidas da resposta
+                    include: [
+                        {
+                            model: Usuario,
+                            as: "autorLike", // Inclui o usuário que curtiu a resposta
+                        }
+                    ]
+                }
+            ],
+        });
+
+        if (!resposta) {
+            throw new Error("Resposta não encontrada.");
+        }
+
         return resposta;
-    }).catch((error) => {
-        console.error("Erro ao criar resposta:", error);
+    } catch (error) {
+        console.error("Erro ao buscar resposta:", error);
         throw error;
-    });
+    }
 }
 
 module.exports = lerRespostaPorID;
