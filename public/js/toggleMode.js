3f13db692b324a7831e57a882eb714e64ffd7136
Matheuz-cmd <teteunv@gmail.com>
2024-08-07 17:08:11 -0300

    Adiciona toggleTheme e Estiliza botões

--- public/js/toggleMode.js
+++ public/js/toggleMode.js
@@ -1,5 +1,28 @@
-function toggleMode(){
+document.addEventListener('DOMContentLoaded', (event) => {
     const html = document.documentElement;
-    html.classList.toggle('light');
+    const moonIcon = document.getElementById('dark-icon');
+    const sunIcon = document.getElementById('light-icon')
+    const savedTheme = localStorage.getItem('theme');
 
-}
+    if (savedTheme) {
+        html.classList.add(savedTheme);
+        if(savedTheme === 'light'){
+            moonIcon.classList.add('visually-hidden');
+            sunIcon.classList.remove('visually-hidden');
+        }
+    }
+
+    window.toggleMode = function() {
+        const isLight = html.classList.toggle('light');
+        if (isLight) {
+            localStorage.setItem('theme', 'light');
+            moonIcon.classList.add('visually-hidden');
+            sunIcon.classList.remove('visually-hidden');
+        } else {
+            localStorage.removeItem('theme');
+            moonIcon.classList.remove('visually-hidden');
+            sunIcon.classList.add('visually-hidden');
+        }
+    };
+
+});
